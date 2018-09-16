5b6cf015e38b8a2dde56454b443f50c56c5f15e0
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-16 20:36:57 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,8 @@
 def my_collect(array)
-  i = 0
-  while i < array.length
-    yield array[i]
-    i = i + 1
+  array = []
+  counter = 0
+  my_collect(array) do |x|
+    counter += 1
   end
-  array
-end
 
 end

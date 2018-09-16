cab19e8614781873ea8ec73e766c5c3c84f19aa6
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-16 20:41:06 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(array)
+  empty_array = []
+  counter = 0
 
-
+  while counter < array.length
+    new_array = yield array[counter]
+    counter += 1
+    empty_array << new_array
+  end
+  empty_array
+end
